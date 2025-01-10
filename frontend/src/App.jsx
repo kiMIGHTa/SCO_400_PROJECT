d4179bc2b18e3d45c17ea96418b70884830707c1
Dennis Kimaita <dkimaita22@gmail.com>
2025-01-10 14:37:43 +0300

    set up app for routing  using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 import Navbar from './components/Navbar/navbar.jsx'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/home.jsx'
+import Cart from './pages/Cart/cart.jsx'
+import placeOrder from './pages/PlaceOrder/placeOrder.jsx'
 function App() {  
   return (
     <div className='app'>
     <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='/placeOrder' element={<placeOrder/>} />
+    </Routes>
     </div>
   )
 }
