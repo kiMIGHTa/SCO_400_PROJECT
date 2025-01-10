824ef6750285befb708698dad9b211bd09ca6eca
Dennis Kimaita <dkimaita22@gmail.com>
2025-01-10 12:50:19 +0300

    create navbar

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -5,8 +5,21 @@ import { assets } from '../../../../downloads/assets/frontend_assets/assets'
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="logo" />
-      Hello world!
+      <img src={assets.logo} alt="logo" className='logo'/>
+      <ul className="navbar-list">
+        <li className="navbar-item">home</li>
+        <li className="navbar-item">menu</li>
+        <li className="navbar-item">mobile app</li>
+        <li className="navbar-item">contact us</li>
+      </ul>
+      <div className='navbar-right'>
+        <img src={assets.search_icon} alt='Search'/>
+        <div className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt='Basket'/>
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
     </div>
   )
 }
