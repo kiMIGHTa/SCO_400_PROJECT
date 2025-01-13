fc61a2bfd70d52cfeef3e3d6a095c01845579941
Dennis Kimaita <dkimaita22@gmail.com>
2025-01-13 14:02:34 +0300

    add state variable on classnames for navbar styling

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { assets } from '../../../../downloads/assets/frontend_assets/assets'
 
 const Navbar = () => {
+
+  const [navSelect, setNavSelect] = useState("home")
+
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="logo" className='logo'/>
       <ul className="navbar-list">
-        <li className="navbar-item">home</li>
-        <li className="navbar-item">menu</li>
-        <li className="navbar-item">mobile app</li>
-        <li className="navbar-item">contact us</li>
+        <li onClick={()=> setNavSelect("home")} className={navSelect==="home"?"active":""}>home</li>
+        <li onClick={()=> setNavSelect("menu")} className={navSelect==="menu"?"active":""}>menu</li>
+        <li onClick={()=> setNavSelect("mobile app")} className={navSelect==="mobile app"?"active":""}>mobile app</li>
+        <li onClick={()=> setNavSelect("contact us")} className={navSelect==="contact us"?"active":""}>contact us</li>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt='Search'/>
-        <div className='navbar-search-icon'>
+        <div className='navbar-basket-icon'>
           <img src={assets.basket_icon} alt='Basket'/>
           <div className="dot"></div>
         </div>
