4b02896f2ed4df89e408908e2c44e816dc4eb843
Dennis Kimaita <dkimaita22@gmail.com>
2025-01-17 14:33:31 +0300

    create cartItems variable for item card display create addToCart and removeFromCart funstions to toggle count of cartItems add to contextValue set

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,8 +4,29 @@ import { food_list } from "../../../downloads/assets/frontend_assets/assets";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props)=>{
+
+    const [cartItems, setCartItems]=useState({})
+
+    const addToCart = (itemId)=>{
+        if (!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else {
+            setCartItems((prev=>({...prev,[itemId]:prev[itemId]+1})))
+        }
+    }
+
+    const removeFromCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
 
     return (
