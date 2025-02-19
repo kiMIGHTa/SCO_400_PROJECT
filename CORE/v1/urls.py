58045134fdec4718cb5124c89a8bfd47bfedc45b
Dennis Kimaita <dkimaita22@gmail.com>
2025-02-19 13:47:48 +0300

    configure to handle image files

--- CORE/v1/urls.py
+++ CORE/v1/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from .views import Hello
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('', Hello.as_view(), name='hello API'),
     path('auth/', include("v1.auth.urls")),
-]
+    path('restaurant/', include("v1.restaurant.urls")),
+    path('food/', include("v1.food.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
