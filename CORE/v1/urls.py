f32c16b51f6a8d06d98b55aa9b4ed1db5f5d60a6
Dennis Kimaita <dkimaita22@gmail.com>
2025-03-08 19:27:41 +0300

    define v1 urls

--- CORE/v1/urls.py
+++ CORE/v1/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', Hello.as_view(), name='hello API'),
     path('auth/', include("v1.auth.urls")),
-    path('restaurant/', include("v1.restaurant.urls")),
+    path('restaurants/', include("v1.restaurant.urls")),
+    path('order/', include("v1.order.urls")),
+    path('cart/', include("v1.cart.urls")),
     path('food/', include("v1.food.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
