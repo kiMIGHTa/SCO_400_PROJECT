a0fef08f8d1ff9b9c0f927d5d7d20cd4d2fc71d6
Dennis Kimaita <dkimaita22@gmail.com>
2025-03-10 14:58:02 +0300

    import status url

--- CORE/v1/payment/urls.py
+++ CORE/v1/payment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import mpesa_validation, mpesa_confirmation, initiate_payment
+from .views import mpesa_validation, mpesa_confirmation, initiate_payment, check_payment_status
 
 urlpatterns = [
     path("validation/", mpesa_validation, name="mpesa-validation"),
     path("confirmation/", mpesa_confirmation, name="mpesa-confirmation"),
     path("simulate/", initiate_payment, name="mpesa-simulate"),
+    path("status/", check_payment_status, name="mpesa-check-payment-status"),  # New endpoint
+
 ]
