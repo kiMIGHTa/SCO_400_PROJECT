9e39370052101d01c5a9ed77cf3524d861f2b0ee
Dennis Kimaita <dkimaita22@gmail.com>
2025-03-23 10:39:07 +0300

    fix '/order/' in the url

--- frontend/src/apiUtils/order/index.js
+++ frontend/src/apiUtils/order/index.js
@@ -2,27 +2,28 @@ import { axiosInstance } from "../api";
 
 // Get all orders for a user
 export const getOrders = async () => {
-  const response = await axiosInstance.get("/orders/");
+  const response = await axiosInstance.get("/order/");
   return response.data;
 };
 
 // Create an order (Checkout)
-export const createOrder = async (cartId, totalPrice) => {
-  const response = await axiosInstance.post("/orders/create/", {
-    cart: cartId,
-    total_price: totalPrice,
-  });
-  return response.data;
+export const createOrder = async (orderDetails) => {
+  const response = await axiosInstance.post("/order/create/", orderDetails);
+  console.log("Response:", response); // ✅ Correct logging
+  console.log("Response Data:", response.data); // ✅ Access response body
+  return response;
 };
 
 // Get order details
 export const getOrderDetails = async (orderId) => {
-  const response = await axiosInstance.get(`/orders/${orderId}/`);
+  const response = await axiosInstance.get(`/order/${orderId}/`);
+  console.log(response.data);
+
   return response.data;
 };
 
 // Mark order as delivered (clears cart)
 export const completeOrder = async (orderId) => {
-  const response = await axiosInstance.patch(`/orders/${orderId}/complete/`);
+  const response = await axiosInstance.patch(`/order/${orderId}/complete/`);
   return response.data;
 };
